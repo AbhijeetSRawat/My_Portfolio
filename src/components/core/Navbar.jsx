6ae3a3f9c3412b1b03569d959660661fe32b458a
Abhijeet Singh Rawat <rawatab2@gmail.com>
2025-05-05 15:17:00 +0530

    dashboard page completed

--- src/components/core/Navbar.jsx
+++ src/components/core/Navbar.jsx
@@ -10,19 +10,22 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
+    <div >
         
         
 
-        <div className='h-16 relative bg-black flex p-2 justify-around text-white text-xl items-center lg:flex-col lg:w-20 lg:justify-center lg:gap-10 lg:h-full'>
+        <div className='h-16 relative bg-black flex p-2 justify-around text-white text-xl items-center lg:flex-col lg:w-20 lg:justify-center lg:gap-10 lg:h-[100vh] translate-x-[-100%] opacity-0 transition-all duration-500 ease-out animate-[slide-in_1.5s_forwards] '>
+            
+            <NavLink to='/'>
+                <img src={logo} className=' lg:absolute top-5 left-3 h-14 w-14 lg:h-14 lg:w-14 rounded-full flip-card' alt="Abhijeet" />
 
-            <img src={logo} className=' lg:absolute top-5 left-3 h-14 w-14 lg:h-14 lg:w-14 rounded-full' alt="Abhijeet" />
+            </NavLink>
         
 
             <NavLink to="/" className='relative text-xl font-semibold rounded-full h-11 w-11 flex justify-center group items-center bg-[#4e4e4e] hover:bg-yellow-500 lg:h-14 lg:w-14 transition-all duration-1000 ease-in-out'>
                 
                 <IoHome />
-                <div className="absolute gap-6 left-0 top-0 w-[100px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out lg:group-hover:translate-x-16 transform lg:group-hover:scale-100  flex pl-[18px] gap-4 items-center">
+                <div className="absolute z-[999]  left-0 top-0 w-[100px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out lg:group-hover:translate-x-16 transform lg:group-hover:scale-100  flex pl-[18px] gap-4 items-center">
                  HOME
                 </div>
                 
@@ -33,7 +36,7 @@ const Navbar = () => {
             <NavLink to="/about" className='relative text-xl font-semibold rounded-full h-11 w-11 flex justify-center group items-center bg-[#4e4e4e] hover:bg-yellow-500 lg:h-14 lg:w-14 transition-all duration-1000 ease-in-out'>
                 
                 <FaUser />
-                <div className="absolute gap-6 left-0 top-0 w-[145px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out transform lg:group-hover:scale-100 lg:group-hover:translate-x-16 flex pl-[18px] gap-4 items-center">
+                <div className="absolute  left-0 top-0 w-[145px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out transform lg:group-hover:scale-100 lg:group-hover:translate-x-16 flex pl-[18px] gap-4 items-center">
                  ABOUT ME
                 </div>
                 
@@ -42,7 +45,7 @@ const Navbar = () => {
             <NavLink to="/projects" className='relative text-xl font-semibold rounded-full h-11 w-11 flex justify-center group items-center bg-[#4e4e4e] hover:bg-yellow-500 lg:h-14 lg:w-14 transition-all duration-1000 ease-in-out'>
                 
                 <FaBriefcase />
-                <div className=" absolute gap-6 left-0 top-0 w-[170px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out transform lg:group-hover:scale-100 lg:group-hover:translate-x-16 flex pl-[18px] gap-4 items-center">
+                <div className=" absolute  left-0 top-0 w-[170px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out transform lg:group-hover:scale-100 lg:group-hover:translate-x-16 flex pl-[18px] gap-4 items-center">
                  MY PROJECTS
                 </div>
                 
@@ -51,7 +54,7 @@ const Navbar = () => {
             <NavLink to="/contact" className='relative text-xl font-semibold rounded-full h-11 w-11 flex justify-center group items-center bg-[#4e4e4e] hover:bg-yellow-500 lg:h-14 lg:w-14 transition-all duration-1000 ease-in-out'>
                 
                 <FaEnvelopeOpen />
-                <div className="absolute gap-6 left-0 top-0 w-[170px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out transform lg:group-hover:scale-100 lg:group-hover:translate-x-16 flex pl-[18px] gap-4 items-center">
+                <div className="absolute  left-0 top-0 w-[170px] rounded-full h-14 bg-yellow-500 opacity-0 invisible lg:group-hover:visible lg:group-hover:opacity-100 transition-all duration-1000 ease-in-out transform lg:group-hover:scale-100 lg:group-hover:translate-x-16 flex pl-[18px] gap-4 items-center">
                  CONTACT ME
                 </div>
                 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
+import portfolio from '../assets/portfoliopic.png'
+import { FaArrowRightLong } from "react-icons/fa6";
 
+import portfoliosm from '../assets/portfoliopicsm.png'
 export default function Dashboard() {
   return (
-    <div className=' bg-[rgba(14,12,12,0.9)]'>
-        this is the dashboard page
+    <div className=' text-white lg:w-full lg:h-full flex lg:justify-between flex-col lg:flex-row items-center lg:p-5 lg:pl-16'>
+        <div className='w-[65vw] h-[65vw] lg:w-[33vw] lg:h-[82vh] mt-10 flex justify-center items-center bg-yellow-500 rounded-full lg:rounded-3xl shadow-lg'>
+          {/* the image div */}
+              <img className='rounded-full w-[60vw] h-[60vw] lg:hidden lg:opacity-0 ' src={portfoliosm} alt="Abhijeet" />
+              <img className='hidden opacity-0 lg:flex lg:opacity-100 rounded-3xl w-[30vw] h-[75vh] shadow-lg  translate-x-[-100%]  transition-all duration-500 ease-out animate-[slide-in_2s_forwards] ' src={portfolio} alt="Abhijeet" />
+        </div>
+        <div className='mt-10 flex flex-col items-center lg:items-start lg:w-[50vw] lg:pl-5 translate-x-[200%] opacity-0 transition-all duration-500 ease-out animate-[slide-in_1.5s_forwards] '>
+
+            <div className='text-2xl font-bold text-yellow-500 lg:text-5xl lg:font-semibold  lg:mb-5'>
+              I'M ABHIJEET SINGH RAWAT.
+            </div>
+            <div className='text-2xl font-bold my-2 lg:text-5xl lg:font-semibold lg:mb-5'>
+              FULL-STACK WEB DEVELOPER
+            </div>
+            <div className='text-center px-3 mt-1 mb-5 lg:text-2xl lg:mb-5 lg:text-start  font-serif'>
+              I am a Full-Stack Web Developer focused on crafting clean and user-friendly experiences, I am passionate about building excellent software that improves the lives of those around me. 
+            </div>
+            
+            <Link to="/about">
+                <div className='flex pl-10 justify-between items-center h-[8vh] w-[70vw] lg:w-[20vw] font-semibold rounded-full border-2 border-yellow-500'>
+                  <div className=''>
+                  MORE ABOUT ME
+                    </div> 
+
+                  <div className='h-[8vh] w-[8vh] flex justify-center items-center bg-yellow-500 rounded-full'>
+                    <FaArrowRightLong className='transition-all duration-200  
+                          motion-safe:animate-[slide_1s_linear_infinite]
+                          ' />
+                  </div>
+                </div>
+            </Link>
+
+        </div>
     </div>
   )
 }
